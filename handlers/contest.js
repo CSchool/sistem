bd56f554cf234601a78d803dd23cdd97d011023d
TsarN (Nikita Tsarev) <nikts53@gmail.com>
2016-02-10 20:30:14 +0300

    Small changes

--- handlers/contest.js
+++ handlers/contest.js
@@ -8,55 +8,82 @@ var fs = require("fs");
 var contests = require("../database/contests");
 var problems = require("../database/problems");
 var session = require("../session");
+var formidable = require("formidable");
 var _ = require("lodash");
 
 function handleContest(response, request) {
     var isAuthorized = session.isAuthorized(request, response);
+    var isAdmin = session.isAdmin(request, response);
     response.writeHead(200, {
         "Content-type": "text/html"
     });
-    api.writeContents(response, request, function(response, callback) {
-        var isAdmin = session.isAdmin(request, response);
-        var now = new Date();
-        var query = url.parse(request.url, true).query;
-        var id = null;
-        if (query.id)
-            id = query.id;
-        if (!isAuthorized) {
-            response.write('<p><div class="error">У Вас нет доступа к запрашиваемой странице</div></p>');
-            callback();
-            return;
-        }
-        contests.Contest.find({_id: id})
-        .populate('problems')
-        .exec(function(err, result) {
-            if (result) {
-                if ((result[0].startTime > now) && !isAdmin) {
-                    response.write('<p><div class="error">У Вас нет доступа к запрашиваемой странице</div></p>');
-                    callback();
-                    return;
-                }
-                var problems = result[0].problems;
-                problems.sort(function(a, b) {
-                    return (a.name - b.name) ;
-                })
-                fs.readFile("frontend/templates/contest.html", "utf8", function(err, data) {
-                    if (err)
-                        throw err;
-                    response.write(_.template(data)({
-                        problems: problems,
-                        isAdmin: isAdmin,
-                        contestId: id,
-                        contest: result[0]
-                    }));
-                    callback();
-                });
-            } else {
-                response.write('<p><div class="error">Неверный ID турнира</div></p>');
+    var query = url.parse(request.url, true).query;
+    var id = null;
+    if (query.id)
+        id = query.id;
+
+    var action = "participate";
+    if (query.action === "manage")
+        action = "manage";
+    if (query.action === "delete")
+        action = "delete";
+
+    var renderPage = function() {
+        api.writeContents(response, request, function(response, callback) {
+            var now = new Date();
+            if (!isAuthorized) {
+                response.write('<p><div class="error">У Вас нет доступа к запрашиваемой странице</div></p>');
                 callback();
+                return;
             }
+            contests.Contest.find({_id: id})
+            .populate('problems')
+            .exec(function(err, result) {
+                if (result) {
+                    if ((result[0].startTime > now) && !isAdmin) {
+                        response.write('<p><div class="error">У Вас нет доступа к запрашиваемой странице</div></p>');
+                        callback();
+                        return;
+                    }
+                    var problems = result[0].problems;
+                    problems.sort(function(a, b) {
+                        return (a.name - b.name) ;
+                    })
+                    fs.readFile("frontend/templates/contest.html", "utf8", function(err, data) {
+                        if (err)
+                            throw err;
+                        response.write(_.template(data)({
+                            problems: problems,
+                            isAdmin: isAdmin,
+                            contestId: id,
+                            contest: result[0]
+                        }));
+                        callback();
+                    });
+                } else {
+                    response.write('<p><div class="error">Неверный ID турнира</div></p>');
+                    callback();
+                }
+            })
+        });
+    };
+
+    if (isAdmin && (action === "delete")) {
+        contests.Contest.remove({_id: id}, function(err) {
+            if (err)
+                throw err;
+            response.writeHead(302, {
+                "Content-type": "text/html",
+                "Location": "/"
+            })
         })
-    });
+    } else {
+        if (request.method.toLowerCase() === "post") {
+            if (isAdmin) {
+                var form = formidable.IncomingForm();
+            }
+        }
+    }
 }
 
 exports.handlersMap = {
--- handlers/problem.js
+++ handlers/problem.js
@@ -12,161 +12,180 @@ var mongoose = require("mongoose");
 
 function handleProblem(response, request) {
     var isAuthorized = session.isAuthorized(request, response);
-    var isAdmin = session.isAdmin(request, response);
-    var renderPage = function() {
-        response.writeHead(200, {
-            "Content-type": "text/html"
-        })
-        api.writeContents(response, request, function(response, callback) {
-            var now = new Date();
-            var query = url.parse(request.url, true).query;
-            var id = null;
-            if (query.problemId)
-                id = query.problemId;
-            var contestId = null;
-            if (query.contestId)
-                contestId = query.contestId;
-            var action = "participate";
-            if (query.action == "manage")
-                action = "manage";
-            if ((!isAuthorized) || (!isAdmin && (action !== "participate"))) {
-                response.write('<p><div class="error">У Вас нет доступа к запрашиваемой странице</div></p>');
-                callback();
-                return;
+    var isAdmin = session.isAdmin(request, response); 
+
+    var query = url.parse(request.url, true).query;
+    var id = null;
+    if (query.problemId)
+        id = query.problemId;
+    var contestId = null;
+    if (query.contestId)
+        contestId = query.contestId;
+    var action = "participate";
+    if (query.action === "manage")
+        action = "manage";
+    if (query.action === "delete")
+        action = "delete";
+
+    if (isAdmin && (action === "delete")) {
+        problems.Problem.remove({_id: id}, function(err) {
+            if (err) {
+                throw err;
             }
-            problems.Problem.find({_id: id}, function(err, result) {
-                if ((result) || ((id === "-1") && (action === "manage"))) {
-                    var problem;
-                    if (result) {
-                        problem = result[0]
-                    } else {
-                        problem = {
-                            name: "",
-                            timeLimit: 2000,
-                            memoryLimit: 65000,
-                            statement: ""
-                        }
-                    }
-
-                    if (action == "participate") {
-                        response.write(`
-                            <h1 class="center">${problem.name}</h1>
-                            <p>
-                                Лимит времени: <i>${problem.timeLimit}мс</i><br />
-                                Лимит памяти: <i>${problem.memoryLimit}КБ</i><br />
-                            </p>
-                            <p>
-                                ${problem.statement}
-                            </p>
-                        `);
-                    } else {
-                        response.write(`
-                            <p>
-                            <form method="POST">
-                                <input type="hidden" name="problemId" value="${id}" />
-                                <input type="hidden" name="contestId" value="${contestId}">
-                                <b>Название задачи:</b>
-                                <br />
-                                <input type="text" name="problemName" value="${problem.name}" />
-                                <br /><br />
-                                <b>Лимит времени(мс):</b>
-                                <br />
-                                <input type="number" name="problemTime" value="${problem.timeLimit}" />
-                                <br /><br />
-                                <b>Лимит памяти(КБ):</b>
-                                <br />
-                                <input type="number" name="problemMemory" value="${problem.memoryLimit}" />
-                                <br /><br />
-                                <b>Условие (поддерживается HTML синтаксис):</b>
-                                <br />
-                                <textarea name="problemStatement">${problem.statement}</textarea>
-                                <br /><br />
-                                <button type="submit" name="action" value="save">Сохранить</button>
-                            </p>
-                            </form>
-                        `)
-                    }
-                } else {
-                    response.write('<p><div class="error">Неверный ID задачи</div></p>');
-                }
-                callback();
-            })
-        })
-    };
-
-    if (request.method.toLowerCase() == "post") {
-        if (isAdmin) {
-            var form = new formidable.IncomingForm();
-            form.parse(request, function(err, fields, files) {
-                if (!fields.problemId) {
-                    response.writeHead(302, {
-                        "Content-type": "text/html",
-                        "Location": "/"
-                    })
-                    response.end();
-                    return;
-                }
-                var name = fields.problemName || "null";
-                var time = fields.problemTime || 2000;
-                var memory = fields.problemMemory || 2000;
-                var statement = fields.problemStatement || "No statement";
-
-                var id = fields.problemId;
-                if (id == "-1")
-                    id = undefined
-
-                var contestId = fields.contestId;
-
-                problems.Problem.findOneAndUpdate({
-                        "_id": mongoose.Types.ObjectId(id)
-                    }, {
-                        "name": name,
-                        "timeLimit": time,
-                        "memoryLimit": memory,
-                        "statement": statement
-                    }, {
-                        "upsert": true,
-                        "new": true
-                    }, function(err, problem) {
-                        if (err)
-                            throw err;
-                        if (!id) {
-                            id = problem._id;
-                            contests.Contest.findOneAndUpdate({
-                                "_id": mongoose.Types.ObjectId(contestId)
-                            }, {
-                                $push: {
-                                    "problems": mongoose.Types.ObjectId(id)
-                                }
-                            }, {
-                                new: true
-                            }, function() {
-                                if (err)
-                                    throw err;
-                                response.writeHead(302, {
-                                    "Content-type": "text/html",
-                                    "Location": "/problem?problemId=" + id + "&action=manage"
-                                    })
-                                response.end();
-                            })
-                        } else {
-                            response.writeHead(302, {
-                                "Content-type": "text/html",
-                                "Location": "/problem?problemId=" + id + "&action=manage"
-                            })
-                            response.end();
-                        }
-                })
-            }) 
-        } else {
             response.writeHead(302, {
                 "Content-type": "text/html",
-                "Location" : "/"
-            })
+                "Location": (contestId) ? ("/contest?id=" + contestId) : ("/")
+            });
             response.end();
-        }
+        })
     } else {
-        renderPage();
+        var renderPage = function() {
+            response.writeHead(200, {
+                "Content-type": "text/html"
+            })
+            api.writeContents(response, request, function(response, callback) {
+                var now = new Date();
+                    contestId = query.contestId;
+                if ((!isAuthorized) || (!isAdmin && (action !== "participate"))) {
+                    response.write('<p><div class="error">У Вас нет доступа к запрашиваемой странице</div></p>');
+                    callback();
+                    return;
+                }
+                problems.Problem.find({_id: id}, function(err, result) {
+                    if ((result) || ((id === "-1") && (action === "manage"))) {
+                        var problem;
+                        if (result) {
+                            problem = result[0]
+                        } else {
+                            problem = {
+                                name: "",
+                                timeLimit: 2000,
+                                memoryLimit: 65000,
+                                statement: ""
+                            }
+                        }
+
+                        if (action == "participate") {
+                            response.write(`
+                                <h1 class="center">${problem.name}</h1>
+                                <p>
+                                    Лимит времени: <i>${problem.timeLimit}мс</i><br />
+                                    Лимит памяти: <i>${problem.memoryLimit}КБ</i><br />
+                                </p>
+                                <p>
+                                    ${problem.statement}
+                                </p>
+                            `);
+                        } else {
+                            response.write(`
+                                <p>
+                                <form method="POST">
+                                    <input type="hidden" name="problemId" value="${id}" />
+                                    <input type="hidden" name="contestId" value="${contestId}">
+                                    <b>Название задачи:</b>
+                                    <br />
+                                    <input type="text" name="problemName" value="${problem.name}" />
+                                    <br /><br />
+                                    <b>Лимит времени(мс):</b>
+                                    <br />
+                                    <input type="number" name="problemTime" value="${problem.timeLimit}" />
+                                    <br /><br />
+                                    <b>Лимит памяти(КБ):</b>
+                                    <br />
+                                    <input type="number" name="problemMemory" value="${problem.memoryLimit}" />
+                                    <br /><br />
+                                    <b>Условие (поддерживается HTML синтаксис):</b>
+                                    <br />
+                                    <textarea name="problemStatement">${problem.statement}</textarea>
+                                    <br /><br />
+                                    <button type="submit" name="action" value="save">Сохранить</button>
+                                </p>
+                                </form>
+                            `)
+                        }
+                    } else {
+                        response.write('<p><div class="error">Неверный ID задачи</div></p>');
+                    }
+                    callback();
+                })
+            })
+        };
+
+        if (request.method.toLowerCase() === "post") {
+            if (isAdmin) {
+                var form = new formidable.IncomingForm();
+                form.parse(request, function(err, fields, files) {
+                    if (!fields.problemId) {
+                        response.writeHead(302, {
+                            "Content-type": "text/html",
+                            "Location": "/"
+                        })
+                        response.end();
+                        return;
+                    }
+                    var name = fields.problemName || "null";
+                    var time = fields.problemTime || 2000;
+                    var memory = fields.problemMemory || 2000;
+                    var statement = fields.problemStatement || "No statement";
+
+                    var id = fields.problemId;
+                    if (id == "-1")
+                        id = undefined
+
+                    var contestId = fields.contestId;
+
+                    problems.Problem.findOneAndUpdate({
+                            "_id": mongoose.Types.ObjectId(id)
+                        }, {
+                            "name": name,
+                            "timeLimit": time,
+                            "memoryLimit": memory,
+                            "statement": statement
+                        }, {
+                            "upsert": true,
+                            "new": true
+                        }, function(err, problem) {
+                            if (err)
+                                throw err;
+                            if (!id) {
+                                id = problem._id;
+                                contests.Contest.findOneAndUpdate({
+                                    "_id": mongoose.Types.ObjectId(contestId)
+                                }, {
+                                    $push: {
+                                        "problems": mongoose.Types.ObjectId(id)
+                                    }
+                                }, {
+                                    new: true
+                                }, function() {
+                                    if (err)
+                                        throw err;
+                                    response.writeHead(302, {
+                                        "Content-type": "text/html",
+                                        "Location": (contestId) ? ("/contest?id=" + contestId) : ("/")
+                                        })
+                                    response.end();
+                                })
+                            } else {
+                                response.writeHead(302, {
+                                    "Content-type": "text/html",
+                                    "Location": (contestId) ? ("/contest?id=" + contestId) :
+                                    ("/problem?problemId=" + id + "&action=manage")
+                                })
+                                response.end();
+                            }
+                    })
+                }) 
+            } else {
+                response.writeHead(302, {
+                    "Content-type": "text/html",
+                    "Location" : "/"
+                })
+                response.end();
+            }
+        } else {
+            renderPage();
+        }
     }
 }
 
